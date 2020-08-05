6940616ad6ed22c92f0f978f24374c9de5332bb2
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-05 11:26:11 +0800

    feat: Implemented GET - /drinks route

--- backend/src/api.py
+++ backend/src/api.py
@@ -29,6 +29,20 @@ CORS(app)
 '''
 
 
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+    try:
+        drinks = Drink.query.all()
+        formatted_drinks = [drink.short() for drink in drinks]
+
+        return jsonify({
+            "success": True,
+            "drinks": formatted_drinks
+        })
+    except:
+        abort(422)
+
+
 '''
 @TODO implement endpoint
     GET /drinks-detail
@@ -37,7 +51,6 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
 
 '''
 @TODO implement endpoint
@@ -48,7 +61,6 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
-
 
 '''
 @TODO implement endpoint
@@ -62,7 +74,6 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
-
 '''
 @TODO implement endpoint
     DELETE /drinks/<id>
@@ -74,18 +85,20 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
-
 ## Error Handling
 '''
 Example error handling for unprocessable entity
 '''
+
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
 
 '''
 @TODO implement error handlers using the @app.errorhandler(error) decorator
@@ -102,7 +115,6 @@ def unprocessable(error):
 @TODO implement error handler for 404
     error handler should conform to general task above 
 '''
-
 
 '''
 @TODO implement error handler for AuthError
