10d06f27c0115d9f8ba48199eb5cb77f6a7be8d1
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-09 20:21:33 +0800

    feat: Implemented DELETE /drinks/:id route

--- backend/src/api.py
+++ backend/src/api.py
@@ -137,6 +137,25 @@ def update_drink(drink_id):
 '''
 
 
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(drink_id):
+    drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+    if drink is None:
+        abort(404)
+
+    try:
+        drink.delete()
+
+        return jsonify({
+            'success': True,
+            'delete': drink_id
+        })
+    except:
+        abort(422)
+
+
 ## Error Handling
 
 @app.errorhandler(422)
