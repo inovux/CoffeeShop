4a242eb23e6c4f0578b1788c9d0502cc2ba4699f
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-08-09 15:11:56 +0800

    refactor: raise AuthError instead of abort function

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -73,10 +73,16 @@ def get_token_auth_header():
 
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
-        abort(400)
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permissions not included in JWT.'
+        }, 400)
 
     if permission not in payload['permissions']:
-        abort(403)
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
 
     return True
 
